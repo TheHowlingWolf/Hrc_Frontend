095a3122c065eb74d8a45536b52935aa19ea9f84
Debraj Sengupta <1705232@kiit.ac.in>
2020-05-03 03:56:06 +0530

    1

--- src/core/components/Feature.js
+++ src/core/components/Feature.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Feature = () => {
-  return <div></div>;
+  return (
+    <section className="feature-section-home">
+      <div className="container d-flex h-100">
+        <div className="row w-100 justify-content-center align-items-center">
+          <div className="col align-self-center"></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Feature;
