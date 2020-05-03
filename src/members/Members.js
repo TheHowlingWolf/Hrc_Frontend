61e58321cf8fdfd6680e02af8736617f3b8442a3
Debraj Sengupta <1705232@kiit.ac.in>
2020-05-04 01:23:27 +0530

    1

--- src/members/Members.js
+++ src/members/Members.js
@@ -1,94 +1,99 @@
 import React from "react";
 import { Jumbotron } from "reactstrap";
 // import SelfQ from "../../img/art-educational-paper-phrase-142815.jpg";
+import pic from "../img/naturephotography.jpg";
 
 import "./members.css";
+
 const Members = () => {
   return (
-    <section className="membeers text-light pb-5">
-      <header class="masthead text-white text-center">
-        <div class="container d-flex align-items-center flex-column">
+    <section
+      classNameName="membeers text-light pb-5"
+      style={{ backgroundColor: "white !important" }}
+    >
+      <header className="masthead text-white text-center">
+        <div className="container d-flex align-items-center flex-column">
           <img
-            class="masthead-avatar mb-5 img-fluid rounded-circle"
+            className="masthead-avatar mb-5 img-fluid rounded-circle"
             src="./naturephotography.jpg"
             alt=""
           />
-          <h1 class="masthead-heading text-uppercase mb-0">Name Here</h1>
+          <h1 className="masthead-heading text-uppercase mb-0">Name Here</h1>
 
-          <div class="divider-custom divider-light">
-            <div class="divider-custom-line"></div>
-            <div class="divider-custom-icon">
-              <i class="fa fa-star"></i>
+          <div className="divider-custom divider-light">
+            <div className="divider-custom-line"></div>
+            <div className="divider-custom-icon">
+              <i className="fa fa-star"></i>
             </div>
-            <div class="divider-custom-line"></div>
+            <div className="divider-custom-line"></div>
           </div>
 
-          <p class="masthead-subheading font-weight-bold mb-0">
+          <p className="masthead-subheading font-weight-bold mb-0">
             Member of House Helsinki
           </p>
-          <p className="lead pt-3">-- Since 2020 --</p>
+          <p classNameName="lead pt-3">-- Since 2020 --</p>
         </div>
       </header>
 
-      <div class="container">
-        <div class="content-section-heading text-center">
-          <h3 class="text-secondary mb-0">Portfolio</h3>
-          <h2 class="mb-5">Recent Projects</h2>
+      <div className="container">
+        <div className="content-section-heading text-center">
+          <h3 className="text-secondary mb-0">Portfolio</h3>
+          <h2 className="mb-5">Recent Projects</h2>
         </div>
-        <div class="row no-gutters">
-          <div class="col-lg-6">
-            <a class="portfolio-item" href="#">
-              <span class="caption">
-                <span class="caption-content">
+        <div className="row no-gutters">
+          <div className="col-lg-6">
+            <a className="portfolio-item" href="#">
+              <span className="caption">
+                <span className="caption-content">
                   <h2>Stationary</h2>
-                  <p class="mb-0">
+                  <p className="mb-0">
                     A yellow pencil with envelopes on a clean, blue backdrop!
                   </p>
                 </span>
               </span>
-              <img class="img-fluid" src="./naturephotography.jpg" alt="" />
+              <img className="img-fluid" src={pic} alt="" />
             </a>
           </div>
-          <div class="col-lg-6">
-            <a class="portfolio-item" href="#">
-              <span class="caption">
-                <span class="caption-content">
+          <div className="col-lg-6">
+            <a className="portfolio-item" href="#">
+              <span className="caption">
+                <span className="caption-content">
                   <h2>Ice Cream</h2>
-                  <p class="mb-0">
+                  <p className="mb-0">
                     A dark blue background with a colored pencil, a clip, and a
                     tiny ice cream cone!
                   </p>
                 </span>
               </span>
-              <img class="img-fluid" src="./naturephotography.jpg" alt="" />
+              <img className="img-fluid" src={pic} alt="" />
             </a>
           </div>
-          <div class="col-lg-6">
-            <a class="portfolio-item" href="#">
-              <span class="caption">
-                <span class="caption-content">
+          <div className="col-lg-6">
+            <a className="portfolio-item" href="#">
+              <span className="caption">
+                <span className="caption-content">
                   <h2>Strawberries</h2>
-                  <p class="mb-0">
+                  <p className="mb-0">
                     Strawberries are such a tasty snack, especially with a
                     little sugar on top!
                   </p>
                 </span>
               </span>
-              <img class="img-fluid" src="./naturephotography.jpg" alt="" />
+              <img className="img-fluid" src={pic} alt="" />
             </a>
           </div>
-          <div class="col-lg-6">
-            <a class="portfolio-item" href="#">
-              <span class="caption">
-                <span class="caption-content">
+          <div className="col-lg-6">
+            <a className="portfolio-item" href="#">
+              <span className="caption">
+                <span className="caption-content">
                   <h2>Workspace</h2>
-                  <p class="mb-0">
+                  <p className="mb-0">
                     A yellow workspace with some scissors, pencils, and other
                     objects.
                   </p>
                 </span>
               </span>
-              <img class="img-fluid" src="./naturephotography.jpg" alt="" />
+              <img className="img-fluid" src={pic} alt="" />
             </a>
           </div>
         </div>
