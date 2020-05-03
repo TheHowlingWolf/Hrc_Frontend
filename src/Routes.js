2482e10ce351a4d2dceccf8104620cfdd4722ea8
Debraj Sengupta <1705232@kiit.ac.in>
2020-05-03 23:51:36 +0530

    1

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./core/Home";
 import Registration from "./user/Registration";
 import Member from "./core/Member";
+import IndiMembers from "./core/IndiMembers";
 
 const Routes = () => {
   return (
@@ -10,6 +11,7 @@ const Routes = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/members" component={Member} />
+        <Route exact path="/members/memberId" component={IndiMembers} />
         <Route exact path="/registration" component={Registration} />
       </Switch>
     </Router>
---
+++ src/core/IndiMembers.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Base from "./Base";
+import Members from "../members/Members";
+
+const IndiMembers = () => {
+  return (
+    <Base>
+      <Members></Members>
+    </Base>
+  );
+};
+
+export default IndiMembers;
