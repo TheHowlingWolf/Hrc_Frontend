d3a43ca77a6e653284e161e74df451461b20e1b6
Debraj Sengupta <1705232@kiit.ac.in>
2020-05-04 21:20:58 +0530

    1

--- src/Routes.js
+++ src/Routes.js
@@ -4,15 +4,17 @@ import Home from "./core/Home";
 import Registration from "./user/Registration";
 import Member from "./core/Member";
 import IndiMembers from "./core/IndiMembers";
+import Activity from "./core/Activity";
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Registration} />
-        {/* <Route exact path="/members" component={Member} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/members" component={Member} />
         <Route exact path="/members/:memberId" component={IndiMembers} />
-        <Route exact path="/registration" component={Registration} /> */}
+        <Route exact path="/registration" component={Registration} />
+        <Route exact path="/activity" component={Activity} />
       </Switch>
     </Router>
   );
--- src/core/components/ThreeQ.js
+++ src/core/components/ThreeQ.js
@@ -29,15 +29,19 @@ const ThreeQ = () => {
 
           <div className="col-lg-6 order-lg-1">
             <div className="p-5">
-              <h2 className="display-4 font-weight-bold text-white">
-              About each of you in the House
+              <h2 className="h1 font-weight-bold text-white">
+                About each of you in the House
               </h2>
               <p className="lead pt-2">
-              “Being different is not being odd, it’s being unique”<br/>
--Ruhaan Chand
-<br/>
-              We all have different abilities, interests, talents, experience and backgrounds and we live life distinctly but as a team, we have moved past our differences and accepted our individuality. We have realized that being you is the most amazing and integral part of our beauty.
-Different yet connected.
+                “Being different is not being odd, it’s being unique”
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-Ruhaan Chand
+                <br />
+                We all have different abilities, interests, talents, experience
+                and backgrounds and we live life distinctly but as a team, we
+                have moved past our differences and accepted our individuality.
+                We have realized that being you is the most amazing and integral
+                part of our beauty. Different yet connected.
               </p>
             </div>
           </div>
@@ -50,14 +54,20 @@ Different yet connected.
           </div>
           <div className="col-lg-6">
             <div className="p-5">
-              <h2 className="display-4 font-weight-bold text-white">
-              Unique thing about your House
+              <h2 className="h1 font-weight-bold text-white">
+                Unique thing about your House
               </h2>
               <p className="lead pt-2">
-              “When you have fun then you’re more interested in learning”<br/>
--Magnus Carlsen<br/>
-We as a house believe that work should have a perfect balance of fun and learning and we imply that in every step of our learning. Using our interests to make learning fun is one of the unique thing about our house. Be it ,using memes to understand a concept or singing songs to relax our minds, we have done it all.
-
+                “When you have fun then you’re more interested in learning”
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-Magnus Carlsen
+                <br />
+                We as a house believe that work should have a perfect balance of
+                fun and learning and we imply that in every step of our
+                learning. Using our interests to make learning fun is one of the
+                unique thing about our house. Be it ,using memes to understand a
+                concept or singing songs to relax our minds, we have done it
+                all.
               </p>
             </div>
           </div>
@@ -70,15 +80,22 @@ We as a house believe that work should have a perfect balance of fun and learnin
           </div>
           <div className="col-lg-6 order-lg-1">
             <div className="p-5">
-              <h2 className="display-4 font-weight-bold text-white">
-              Unique thing about you all as a group:
+              <h2 className="h1 font-weight-bold text-white">
+                Unique thing about you all as a group:
               </h2>
               <p className="lead pt-2">
-
-“The achievement of an organization are the results of the combined effort of each individual"<br/>
--Vince Lombardi<br/>
-We are competitors and have been competing with each other every day but as a group we support our members and help them when needed. Be it clearing a doubt or discussing till late at night for weekly tasks. Helping others voluntarily and making sure the task is done before time is what we all do. When needed we come together and stand tall as a group. For us Team always comes first.
-
+                “The achievement of an organization are the results of the
+                combined effort of each individual"
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-Vince Lombardi
+                <br />
+                We are competitors and have been competing with each other every
+                day but as a group we support our members and help them when
+                needed. Be it clearing a doubt or discussing till late at night
+                for weekly tasks. Helping others voluntarily and making sure the
+                task is done before time is what we all do. When needed we come
+                together and stand tall as a group. For us Team always comes
+                first.
               </p>
             </div>
           </div>
